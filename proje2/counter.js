6661d6edc39a7fd18eb6ac4f1e284f8dfd8bd529
Irem845 <n.iremcelik845@gmail.com>
2024-07-15 16:59:49 +0300

    Add files via upload
    first commit

---
+++ proje2/counter.js
@@ -0,0 +1,11 @@
+
+
+var decreaseButtons=document.getElementsByClassName("btn-decrease");
+var increaseButtons=document.getElementsByClassName("btn-increase");
+var resetButton=document.getElementsByClassName("btn-reset");
+var myValue=document.getElementById("value");
+
+
+
+
+
